db6c8be0d2937aab9cb53ddd52d9d18404bf088f
quanbigboss1 <82883115+quanbigboss1@users.noreply.github.com>
2023-04-14 20:10:41 +0700

    Add files via upload

---
+++ Lab_11_Bài 1.sql
@@ -0,0 +1,145 @@
+﻿-- bai1
+-- cau 1
+CREATE TABLE Khoa (
+Makhoa VARCHAR(10) PRIMARY KEY,
+Tenkhoa NVARCHAR(50) NOT NULL,
+Dienthoai VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Lop (
+Malop VARCHAR(10) PRIMARY KEY,
+Tenlop NVARCHAR(50) NOT NULL,
+Khoa VARCHAR(10) NOT NULL,
+Hedt NVARCHAR(50) NOT NULL,
+Namnhaphoc INT NOT NULL,
+Makhoa VARCHAR(10) NOT NULL,
+FOREIGN KEY (Khoa) REFERENCES Khoa(Makhoa),
+FOREIGN KEY (Makhoa) REFERENCES Khoa(Makhoa)
+);
+--cau 2
+CREATE PROCEDURE [dbo].[spInsertKhoa]
+    @MaKhoa VARCHAR(10),
+    @TenKhoa NVARCHAR(100),
+    @DienThoai VARCHAR(20)
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM KHOA WHERE TenKhoa = @TenKhoa)
+    BEGIN
+        PRINT N'Tên khoa đã tồn tại !'
+        RETURN
+    END
+    
+    INSERT INTO KHOA(MaKhoa, TenKhoa, DienThoai)
+    VALUES (@MaKhoa, @TenKhoa, @DienThoai)
+    
+    PRINT 'Thêm khoa thành công!'
+END
+EXEC spInsertKhoa 'K01', N'Khoa A', '0123456789'
+EXEC spInsertKhoa 'K02', N'Khoa A', '0123456789'
+-- cau 2
+CREATE PROCEDURE sp_NhapLop
+    @Malop nvarchar(10),
+    @Tenlop nvarchar(50),
+    @Khoa nvarchar(50),
+    @Hedt nvarchar(50),
+    @Namnhaphoc int,
+    @Makhoa nvarchar(10)
+AS
+BEGIN
+    -- Kiểm tra xem tên lớp đã có trước đó chưa nếu có thì thông báo
+    IF EXISTS (SELECT * FROM Lop WHERE Tenlop = @Tenlop)
+    BEGIN
+        PRINT N'Lớp đã tồn tại'
+        RETURN
+    END
+
+    -- Kiểm tra xem makhoa này có trong bảng khoa hay không nếu không có thì thông báo
+    IF NOT EXISTS (SELECT * FROM Khoa WHERE Makhoa = @Makhoa)
+    BEGIN
+        PRINT N'Mã khoa không tồn tại'
+        RETURN
+    END
+
+    -- Nếu đầy đủ thông tin thì cho nhập
+    INSERT INTO Lop(Malop, Tenlop, Khoa, Hedt, Namnhaphoc, Makhoa)
+    VALUES(@Malop, @Tenlop, @Khoa, @Hedt, @Namnhaphoc, @Makhoa)
+    
+    PRINT N'Nhập thành công'
+END
+EXEC sp_NhapLop 'L01', N'Lớp A', 'K01', N'Cử nhân', 2023, 'K01'
+-- cau 3
+CREATE PROCEDURE sp_InsertKhoa_CheckExists
+    @MaKhoa VARCHAR(10),
+    @TenKhoa NVARCHAR(100),
+    @DienThoai VARCHAR(20),
+    @Exists BIT OUTPUT
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM KHOA WHERE TenKhoa = @TenKhoa)
+    BEGIN
+        SET @Exists = 0
+        RETURN
+    END
+    
+    INSERT INTO KHOA(MaKhoa, TenKhoa, DienThoai)
+    VALUES (@MaKhoa, @TenKhoa, @DienThoai)
+    
+    SET @Exists = 1
+END
+DECLARE @Exists BIT
+EXEC sp_InsertKhoa_CheckExists 'K01', N'Khoa A', '0123456789', @Exists OUTPUT
+IF @Exists = 1
+BEGIN
+    PRINT 'Thêm khoa thành công!'
+END
+ELSE
+BEGIN
+    PRINT N'Tên khoa đã tồn tại !'
+END
+-- cau 4
+CREATE PROCEDURE sp_InsertLop
+    @MaLop VARCHAR(10),
+    @TenLop NVARCHAR(50),
+    @Khoa NVARCHAR(10),
+    @HeDT NVARCHAR(50),
+    @NamNhapHoc INT,
+    @MaKhoa VARCHAR(10)
+AS
+BEGIN
+    -- Kiểm tra xem tên lớp đã có trước đó chưa nếu có thì trả về 0
+    IF EXISTS (SELECT 1 FROM Lop WHERE Tenlop = @TenLop)
+    BEGIN
+        RETURN 0
+    END
+
+    -- Kiểm tra xem makhoa này có trong bảng khoa hay không nếu không có thì trả về 1
+    IF NOT EXISTS (SELECT 1 FROM Khoa WHERE Makhoa = @MaKhoa)
+    BEGIN
+        RETURN 1
+    END
+
+    -- Nếu đầy đủ thông tin thì cho nhập và trả về 2
+    INSERT INTO Lop(Malop, Tenlop, Khoa, Hedt, Namnhaphoc, Makhoa)
+    VALUES(@MaLop, @TenLop, @Khoa, @HeDT, @NamNhapHoc, @MaKhoa)
+    
+    RETURN 2
+END
+DECLARE @Result INT
+
+EXEC @Result = sp_InsertLop 'L09', N'Lớp 9', 'K09', N'DH', 2021, 'K09'
+
+IF @Result = 0
+BEGIN
+    PRINT N'Tên lớp đã có trước đó'
+END
+ELSE IF @Result = 1
+BEGIN
+    PRINT N'Mã khoa không tồn tại'
+END
+ELSE IF @Result = 2
+BEGIN
+    PRINT N'Nhập dữ liệu thành công'
+END
+
+
+
---
+++ Lab_11_Bài 2.sql
@@ -0,0 +1,111 @@
+﻿-- lab_11_câu 2
+--  a)tạo csdl
+create database QLNV
+-- b) tạo các bảng
+CREATE TABLE tblChucvu (
+  MaCV VARCHAR(2) PRIMARY KEY,
+  TenCV NVARCHAR(30) NOT NULL
+);
+
+CREATE TABLE tblNhanVien (
+  MaNV VARCHAR(4) PRIMARY KEY,
+  MaCV VARCHAR(2) NOT NULL,
+  TenNV NVARCHAR(30) NOT NULL,
+  NgaySinh DATE NOT NULL,
+  LuongCanBan float NOT NULL,
+  NgayCong INT NOT NULL,
+  PhuCap float NOT NULL,
+  FOREIGN KEY (MaCV) REFERENCES tblChucvu(MaCV)
+);
+-- insert du lieu
+INSERT INTO tblChucvu (MaCV, TenCV) VALUES ('BV', 'Bảo vệ');
+INSERT INTO tblChucvu (MaCV, TenCV) VALUES ('GD', 'Giám đốc');
+INSERT INTO tblChucvu (MaCV, TenCV) VALUES ('HC', 'Hành Chính');
+INSERT INTO tblChucvu (MaCV, TenCV) VALUES ('KT', 'Kế Toán');
+INSERT INTO tblChucvu (MaCV, TenCV) VALUES ('TQ', 'Thu Quỹ');
+INSERT INTO tblChucvu (MaCV, TenCV) VALUES ('VS', 'Vệ Sinh');
+
+INSERT INTO tblNhanVien (MaNV, MaCV, TenNV, NgaySinh, LuongCanBan, NgayCong, PhuCap)
+VALUES ('NV01', 'GD', 'Nguyễn Văn An', '1977-12-12', 700000, 25, 500000);
+
+INSERT INTO tblNhanVien (MaNV, MaCV, TenNV, NgaySinh, LuongCanBan, NgayCong, PhuCap)
+VALUES ('NV02', 'BV', 'Bùi Văn Tí', '1978-10-10', 400000, 24, 100000);
+
+INSERT INTO tblNhanVien (MaNV, MaCV, TenNV, NgaySinh, LuongCanBan, NgayCong, PhuCap)
+VALUES ('NV03', 'KT', 'Trần Thanh Nhật', '1977-9-9', 600000, 26, 400000);
+
+INSERT INTO tblNhanVien (MaNV, MaCV, TenNV, NgaySinh, LuongCanBan, NgayCong, PhuCap)
+VALUES ('NV04', 'VS', 'Nguyễn Thị Út', '1980-10-10', 300000, 26, 300000);
+
+INSERT INTO tblNhanVien (MaNV, MaCV, TenNV, NgaySinh, LuongCanBan, NgayCong, PhuCap)
+VALUES ('NV05', 'HC', 'Lê Thị Hà', '1979-10-10', 500000, 27, 200000);
+--Yeu cau a
+CREATE PROCEDURE [dbo].[ThemNhanVien]
+    @MaNV VARCHAR(4),
+    @MaCV VARCHAR(2),
+    @TenNV NVARCHAR(30),
+    @NgaySinh DATE,
+    @LuongCanBan FLOAT,
+    @NgayCong INT,
+    @PhuCap FLOAT
+AS
+BEGIN
+    -- Kiểm tra xem MaCV có tồn tại trong bảng tblChucVu hay không
+    IF NOT EXISTS (SELECT * FROM tblChucVu WHERE MaCV = @MaCV)
+    BEGIN
+        PRINT N'Mã chức vụ không tồn tại trong bảng tblChucVu!'
+        RETURN
+    END
+    
+    -- Thêm nhân viên mới vào bảng tblNhanVien
+    INSERT INTO tblNhanVien (MaNV, MaCV, TenNV, NgaySinh, LuongCanBan, NgayCong, PhuCap)
+    VALUES (@MaNV, @MaCV, @TenNV, @NgaySinh, @LuongCanBan, @NgayCong, @PhuCap)
+    
+    PRINT N'Thêm nhân viên thành công!'
+END
+EXEC ThemNhanVien 'NV01', 'CV01', N'Nguyễn Văn A', '1990-01-01', 5000000, 22, 1000000
+-- yeu cau b
+CREATE PROCEDURE SP_CapNhatNhanVien 
+  @MaNV VARCHAR(4),
+  @MaCV VARCHAR(2),
+  @TenNV NVARCHAR(30),
+  @NgaySinh DATE,
+  @LuongCanBan FLOAT,
+  @NgayCong INT,
+  @PhuCap FLOAT
+AS
+BEGIN
+  -- kiểm tra xem MaCV có tồn tại trong tblChucVu hay không
+  IF EXISTS(SELECT * FROM tblChucVu WHERE MaCV = @MaCV)
+  BEGIN
+    -- cập nhật thông tin nhân viên
+    UPDATE tblNhanVien 
+    SET MaCV = @MaCV, 
+        TenNV = @TenNV, 
+        NgaySinh = @NgaySinh, 
+        LuongCanBan = @LuongCanBan, 
+        NgayCong = @NgayCong, 
+        PhuCap = @PhuCap 
+    WHERE MaNV = @MaNV;
+    PRINT N'Đã cập nhật thông tin nhân viên.'
+  END
+  ELSE
+  BEGIN
+    -- thông báo lỗi nếu MaCV không tồn tại trong tblChucVu
+    PRINT N'Mã chức vụ không tồn tại trong bảng chức vụ.'
+  END
+END
+EXEC SP_CapNhatNhanVien 'NV01', 'CV01', 'Nguyen Van A', '1990-01-01', 5000000, 20, 1000000
+-- yeu cau c
+CREATE PROCEDURE SP_LuongLN
+AS
+BEGIN
+  SELECT MaNV, TenNV, LuongCanBan*NgayCong+PhuCap AS Luong
+  FROM tblNhanVien;
+END
+EXEC SP_LuongLN;
+
+
+
+
+
